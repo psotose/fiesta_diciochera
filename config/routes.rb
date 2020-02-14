783277392150517b7f03faf1af368466e82010ed
patito <p.sotosellanes@hotmail.com>
2020-02-14 00:15:50 -0300

    structure and devise

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/show'
+  devise_for :users
+  get 'tasks/index'
+  root to: "tasks#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
